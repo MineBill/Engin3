4357833ca98013816653f0e0cefe39c951c08027
MineBill <minebill@protonmail.com>
2024-04-12 14:05:09 +0300

    Build tracy with TRACY_ON_DEMAND to disable event buildup that cause memory to skyrocket.

--- packages/build.bat
+++ packages/build.bat
@@ -2,19 +2,51 @@
 
 CD /D "%~dp0"
 
+REM Check if an argument is provided
+IF "%1"=="" (
+    REM If no argument is provided, build all
+    CALL :BuildAll
+) ELSE (
+    REM If an argument is provided, build the specified target
+    IF /I "%1"=="tracy" (
+        CALL :BuildTracy
+    ) ELSE IF /I "%1"=="imgui" (
+        CALL :BuildImGui
+    ) ELSE IF /I "%1"=="nuklear" (
+        CALL :BuildNuklear
+    ) ELSE (
+        ECHO Invalid argument. Valid options are: tracy, imgui, nuklear
+    )
+)
+
+REM Exit the script
+EXIT /B
+
+:BuildAll
+REM Build all targets
+CALL :BuildTracy
+CALL :BuildImGui
+CALL :BuildNuklear
+EXIT /B
+
+:BuildTracy
 REM Tracy
 PUSHD odin-tracy
 CALL vcvarsall x64
-cl /MD -O2 -DTRACY_ENABLE -c tracy\public\TracyClient.cpp -Fotracy
+cl /MD -O2 -DTRACY_ENABLE -DTRACY_ON_DEMAND -c tracy\public\TracyClient.cpp -Fotracy
 lib tracy.obj
 del tracy.obj
 POPD
+EXIT /B
 
+:BuildImGui
 REM Dear imgui
 PUSHD odin-imgui
 python3 build.py
 POPD
+EXIT /B
 
+:BuildNuklear
 REM nuklear
 PUSHD odin-nuklear
 CALL vcvarsall x64
@@ -23,3 +55,4 @@ lib nuklear.obj
 del nuklear.obj
 move nuklear.lib nuklear_windows_amd64.lib
 POPD
+EXIT /B
--- packages/build.sh
+++ packages/build.sh
@@ -2,19 +2,55 @@
 
 cd "$(dirname "$0")"
 
-# Tracy
-pushd odin-tracy
-c++ -std=c++11 -DTRACY_ENABLE -O2 tracy/public/TracyClient.cpp -c -fPIC -o tracy.o
-ar rcs libtracy.a tracy.o
-popd
+# Check if an argument is provided
+if [ -z "$1" ]; then
+    # If no argument is provided, build all
+    build_all
+else
+    # If an argument is provided, build the specified target
+    case "$1" in
+        "tracy")
+            build_tracy
+            ;;
+        "imgui")
+            build_imgui
+            ;;
+        "nuklear")
+            build_nuklear
+            ;;
+        *)
+            echo "Invalid argument. Valid options are: tracy, imgui, nuklear"
+            ;;
+    esac
+fi
 
-# Dear imgui
-pushd odin-imgui
-python3 build.py
-popd
+exit 0
 
-# nuklear
-pushd odin-nuklear
-cc -c nuklear.c -std=c99 -o nuklear.o -g
-ar rcs nuklear_linux.a nuklear.o
-popd
+build_all() {
+    build_tracy
+    build_imgui
+    build_nuklear
+}
+
+build_tracy() {
+    # Tracy
+    pushd odin-tracy
+    c++ -std=c++11 -DTRACY_ENABLE -DTRACY_ON_DEMAND -O2 tracy/public/TracyClient.cpp -c -fPIC -o tracy.o
+    ar rcs libtracy.a tracy.o
+    popd
+}
+
+build_imgui() {
+    # Dear imgui
+    pushd odin-imgui
+    python3 build.py
+    popd
+}
+
+build_nuklear() {
+    # nuklear
+    pushd odin-nuklear
+    cc -c nuklear.c -std=c99 -o nuklear.o -g
+    ar rcs nuklear_linux.a nuklear.o
+    popd
+}
